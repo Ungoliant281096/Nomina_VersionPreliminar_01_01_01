26c906dfc4133eded86f90ea75d0fc5dd7b39f0b
Claudia Pérez <claudia.perez1703@outlook.com>
2023-09-05 10:19:03 -0600

    Se modifico el label 2 de "nombre" a "id"

--- AplicaciónNomina_versionPreliminar_23_09_01/Form2.Designer.vb
+++ AplicaciónNomina_versionPreliminar_23_09_01/Form2.Designer.vb
@@ -56,259 +56,291 @@ Partial Class Form2
         Button2 = New Button()
         Button3 = New Button()
         GroupBox1 = New GroupBox()
-        DateTimePicker1 = New DateTimePicker()
         DateTimePicker2 = New DateTimePicker()
+        DateTimePicker1 = New DateTimePicker()
         GroupBox1.SuspendLayout()
         SuspendLayout()
         ' 
         ' TextBox1
         ' 
         TextBox1.CharacterCasing = CharacterCasing.Upper
-        TextBox1.Location = New Point(225, 29)
+        TextBox1.Location = New Point(257, 39)
+        TextBox1.Margin = New Padding(3, 4, 3, 4)
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(227, 23)
+        TextBox1.Size = New Size(259, 27)
         TextBox1.TabIndex = 0
         ' 
         ' TextBox2
         ' 
         TextBox2.CharacterCasing = CharacterCasing.Upper
-        TextBox2.Location = New Point(144, 62)
+        TextBox2.Location = New Point(165, 83)
+        TextBox2.Margin = New Padding(3, 4, 3, 4)
         TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(308, 23)
+        TextBox2.Size = New Size(351, 27)
         TextBox2.TabIndex = 1
         ' 
         ' TextBox3
         ' 
         TextBox3.CharacterCasing = CharacterCasing.Upper
-        TextBox3.Location = New Point(144, 91)
+        TextBox3.Location = New Point(165, 121)
+        TextBox3.Margin = New Padding(3, 4, 3, 4)
         TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(308, 23)
+        TextBox3.Size = New Size(351, 27)
         TextBox3.TabIndex = 2
         ' 
         ' TextBox4
         ' 
         TextBox4.CharacterCasing = CharacterCasing.Upper
-        TextBox4.Location = New Point(144, 120)
+        TextBox4.Location = New Point(165, 160)
+        TextBox4.Margin = New Padding(3, 4, 3, 4)
         TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(308, 23)
+        TextBox4.Size = New Size(351, 27)
         TextBox4.TabIndex = 3
         ' 
         ' TextBox5
         ' 
         TextBox5.CharacterCasing = CharacterCasing.Upper
-        TextBox5.Location = New Point(144, 147)
+        TextBox5.Location = New Point(165, 196)
+        TextBox5.Margin = New Padding(3, 4, 3, 4)
         TextBox5.Name = "TextBox5"
-        TextBox5.Size = New Size(308, 23)
+        TextBox5.Size = New Size(351, 27)
         TextBox5.TabIndex = 4
         ' 
         ' TextBox6
         ' 
         TextBox6.CharacterCasing = CharacterCasing.Upper
-        TextBox6.Location = New Point(144, 176)
+        TextBox6.Location = New Point(165, 235)
+        TextBox6.Margin = New Padding(3, 4, 3, 4)
         TextBox6.Name = "TextBox6"
-        TextBox6.Size = New Size(79, 23)
+        TextBox6.Size = New Size(90, 27)
         TextBox6.TabIndex = 5
         ' 
         ' TextBox7
         ' 
         TextBox7.CharacterCasing = CharacterCasing.Upper
-        TextBox7.Location = New Point(348, 176)
+        TextBox7.Location = New Point(398, 235)
+        TextBox7.Margin = New Padding(3, 4, 3, 4)
         TextBox7.Name = "TextBox7"
-        TextBox7.Size = New Size(79, 23)
+        TextBox7.Size = New Size(90, 27)
         TextBox7.TabIndex = 6
         ' 
         ' TextBox8
         ' 
         TextBox8.CharacterCasing = CharacterCasing.Upper
-        TextBox8.Location = New Point(144, 205)
+        TextBox8.Location = New Point(165, 273)
+        TextBox8.Margin = New Padding(3, 4, 3, 4)
         TextBox8.Name = "TextBox8"
-        TextBox8.Size = New Size(104, 23)
+        TextBox8.Size = New Size(118, 27)
         TextBox8.TabIndex = 7
         ' 
         ' TextBox9
         ' 
         TextBox9.CharacterCasing = CharacterCasing.Upper
-        TextBox9.Location = New Point(348, 205)
+        TextBox9.Location = New Point(398, 273)
+        TextBox9.Margin = New Padding(3, 4, 3, 4)
         TextBox9.Name = "TextBox9"
-        TextBox9.Size = New Size(104, 23)
+        TextBox9.Size = New Size(118, 27)
         TextBox9.TabIndex = 8
         ' 
         ' TextBox10
         ' 
         TextBox10.CharacterCasing = CharacterCasing.Upper
-        TextBox10.Location = New Point(144, 234)
+        TextBox10.Location = New Point(165, 312)
+        TextBox10.Margin = New Padding(3, 4, 3, 4)
         TextBox10.Name = "TextBox10"
-        TextBox10.Size = New Size(104, 23)
+        TextBox10.Size = New Size(118, 27)
         TextBox10.TabIndex = 9
         ' 
         ' TextBox11
         ' 
         TextBox11.CharacterCasing = CharacterCasing.Upper
-        TextBox11.Location = New Point(348, 234)
+        TextBox11.Location = New Point(398, 312)
+        TextBox11.Margin = New Padding(3, 4, 3, 4)
         TextBox11.Name = "TextBox11"
-        TextBox11.Size = New Size(104, 23)
+        TextBox11.Size = New Size(118, 27)
         TextBox11.TabIndex = 10
         ' 
         ' TextBox12
         ' 
         TextBox12.CharacterCasing = CharacterCasing.Upper
-        TextBox12.Location = New Point(144, 263)
+        TextBox12.Location = New Point(165, 351)
+        TextBox12.Margin = New Padding(3, 4, 3, 4)
         TextBox12.Name = "TextBox12"
-        TextBox12.Size = New Size(104, 23)
+        TextBox12.Size = New Size(118, 27)
         TextBox12.TabIndex = 11
         ' 
         ' TextBox13
         ' 
         TextBox13.CharacterCasing = CharacterCasing.Upper
-        TextBox13.Location = New Point(348, 263)
+        TextBox13.Location = New Point(398, 351)
+        TextBox13.Margin = New Padding(3, 4, 3, 4)
         TextBox13.Name = "TextBox13"
-        TextBox13.Size = New Size(104, 23)
+        TextBox13.Size = New Size(118, 27)
         TextBox13.TabIndex = 12
         ' 
         ' TextBox14
         ' 
         TextBox14.CharacterCasing = CharacterCasing.Upper
-        TextBox14.Location = New Point(144, 292)
+        TextBox14.Location = New Point(165, 389)
+        TextBox14.Margin = New Padding(3, 4, 3, 4)
         TextBox14.Name = "TextBox14"
-        TextBox14.Size = New Size(104, 23)
+        TextBox14.Size = New Size(118, 27)
         TextBox14.TabIndex = 13
         ' 
         ' Label1
         ' 
-        Label1.Location = New Point(87, 37)
+        Label1.Location = New Point(99, 49)
         Label1.Name = "Label1"
-        Label1.Size = New Size(51, 15)
+        Label1.Size = New Size(58, 20)
         Label1.TabIndex = 14
-        Label1.Text = "Numero" ' 
+        Label1.Text = "Id"
+        ' 
         ' Label2
         ' 
         Label2.BackColor = Color.FromArgb(CByte(255), CByte(255), CByte(128))
-        Label2.Location = New Point(144, 29)
+        Label2.Location = New Point(165, 39)
         Label2.Name = "Label2"
-        Label2.Size = New Size(41, 23)
+        Label2.Size = New Size(47, 31)
         Label2.TabIndex = 15
         ' 
         ' Label3
         ' 
-        Label3.Location = New Point(87, 70)
+        Label3.Location = New Point(99, 93)
         Label3.Name = "Label3"
-        Label3.Size = New Size(37, 15)
+        Label3.Size = New Size(42, 20)
         Label3.TabIndex = 16
-        Label3.Text = "CURP" ' 
+        Label3.Text = "CURP"
+        ' 
         ' Label4
         ' 
-        Label4.Location = New Point(191, 37)
+        Label4.Location = New Point(218, 49)
         Label4.Name = "Label4"
-        Label4.Size = New Size(28, 15)
+        Label4.Size = New Size(32, 20)
         Label4.TabIndex = 17
-        Label4.Text = "RFC" ' 
+        Label4.Text = "RFC"
+        ' 
         ' Label5
         ' 
-        Label5.Location = New Point(40, 155)
+        Label5.Location = New Point(46, 207)
         Label5.Name = "Label5"
-        Label5.Size = New Size(99, 15)
+        Label5.Size = New Size(113, 20)
         Label5.TabIndex = 20
-        Label5.Text = "Apellido materno" ' 
+        Label5.Text = "Apellido materno"
+        ' 
         ' Label6
         ' 
-        Label6.Location = New Point(44, 128)
+        Label6.Location = New Point(50, 171)
         Label6.Name = "Label6"
-        Label6.Size = New Size(95, 15)
+        Label6.Size = New Size(109, 20)
         Label6.TabIndex = 19
-        Label6.Text = "Apellido paterno" ' 
+        Label6.Text = "Apellido paterno"
+        ' 
         ' Label7
         ' 
-        Label7.Location = New Point(88, 99)
+        Label7.Location = New Point(101, 132)
         Label7.Name = "Label7"
-        Label7.Size = New Size(51, 15)
+        Label7.Size = New Size(58, 20)
         Label7.TabIndex = 18
-        Label7.Text = "Nombre" ' 
+        Label7.Text = "Nombre"
+        ' 
         ' Label8
         ' 
-        Label8.Location = New Point(18, 213)
+        Label8.Location = New Point(21, 284)
         Label8.Name = "Label8"
-        Label8.Size = New Size(120, 15)
+        Label8.Size = New Size(137, 20)
         Label8.TabIndex = 23
-        Label8.Text = "Ingreso diario normal" ' 
+        Label8.Text = "Ingreso diario normal"
+        ' 
         ' Label9
         ' 
-        Label9.Location = New Point(263, 184)
+        Label9.Location = New Point(301, 245)
         Label9.Name = "Label9"
-        Label9.Size = New Size(79, 15)
+        Label9.Size = New Size(90, 20)
         Label9.TabIndex = 22
-        Label9.Text = "Fecha de baja" ' 
+        Label9.Text = "Fecha de baja"
+        ' 
         ' Label10
         ' 
-        Label10.Location = New Point(43, 184)
+        Label10.Location = New Point(49, 245)
         Label10.Name = "Label10"
-        Label10.Size = New Size(96, 15)
+        Label10.Size = New Size(110, 20)
         Label10.TabIndex = 21
-        Label10.Text = "Fecha de ingreso" ' 
+        Label10.Text = "Fecha de ingreso"
+        ' 
         ' Label11
         ' 
-        Label11.Location = New Point(263, 242)
+        Label11.Location = New Point(301, 323)
         Label11.Name = "Label11"
-        Label11.Size = New Size(79, 15)
+        Label11.Size = New Size(90, 20)
         Label11.TabIndex = 26
-        Label11.Text = "Registro IMSS" ' 
+        Label11.Text = "Registro IMSS"
+        ' 
         ' Label12
         ' 
-        Label12.Location = New Point(30, 242)
+        Label12.Location = New Point(34, 323)
         Label12.Name = "Label12"
-        Label12.Size = New Size(108, 15)
+        Label12.Size = New Size(123, 20)
         Label12.TabIndex = 25
-        Label12.Text = "Otros ingresos fijos" ' 
+        Label12.Text = "Otros ingresos fijos"
+        ' 
         ' Label13
         ' 
-        Label13.Location = New Point(256, 213)
+        Label13.Location = New Point(293, 284)
         Label13.Name = "Label13"
-        Label13.Size = New Size(86, 15)
+        Label13.Size = New Size(98, 20)
         Label13.TabIndex = 24
-        Label13.Text = "Viaticos diarios" ' 
+        Label13.Text = "Viaticos diarios"
+        ' 
         ' Label14
         ' 
-        Label14.Location = New Point(9, 300)
+        Label14.Location = New Point(10, 400)
         Label14.Name = "Label14"
-        Label14.Size = New Size(129, 15)
+        Label14.Size = New Size(147, 20)
         Label14.TabIndex = 29
-        Label14.Text = "Salario diario integrado" ' 
+        Label14.Text = "Salario diario integrado"
+        ' 
         ' Label15
         ' 
-        Label15.Location = New Point(279, 271)
+        Label15.Location = New Point(319, 361)
         Label15.Name = "Label15"
-        Label15.Size = New Size(63, 15)
+        Label15.Size = New Size(72, 20)
         Label15.TabIndex = 28
-        Label15.Text = "Porcentaje" ' 
+        Label15.Text = "Porcentaje"
+        ' 
         ' Label16
         ' 
-        Label16.Location = New Point(106, 271)
+        Label16.Location = New Point(121, 361)
         Label16.Name = "Label16"
-        Label16.Size = New Size(33, 15)
+        Label16.Size = New Size(38, 20)
         Label16.TabIndex = 27
-        Label16.Text = "Obra" ' 
+        Label16.Text = "Obra"
+        ' 
         ' Button1
         ' 
-        Button1.Location = New Point(12, 360)
+        Button1.Location = New Point(14, 480)
+        Button1.Margin = New Padding(3, 4, 3, 4)
         Button1.Name = "Button1"
-        Button1.Size = New Size(133, 36)
+        Button1.Size = New Size(152, 48)
         Button1.TabIndex = 30
         Button1.Text = "Guardar"
         Button1.UseVisualStyleBackColor = True
         ' 
         ' Button2
         ' 
-        Button2.Location = New Point(174, 360)
+        Button2.Location = New Point(199, 480)
+        Button2.Margin = New Padding(3, 4, 3, 4)
         Button2.Name = "Button2"
-        Button2.Size = New Size(133, 36)
+        Button2.Size = New Size(152, 48)
         Button2.TabIndex = 31
         Button2.Text = "Limpiar"
         Button2.UseVisualStyleBackColor = True
         ' 
         ' Button3
         ' 
-        Button3.Location = New Point(339, 360)
+        Button3.Location = New Point(387, 480)
+        Button3.Margin = New Padding(3, 4, 3, 4)
         Button3.Name = "Button3"
-        Button3.Size = New Size(133, 36)
+        Button3.Size = New Size(152, 48)
         Button3.TabIndex = 32
         Button3.Text = "Cancelar"
         Button3.UseVisualStyleBackColor = True
@@ -347,35 +379,41 @@ Partial Class Form2
         GroupBox1.Controls.Add(TextBox3)
         GroupBox1.Controls.Add(TextBox2)
         GroupBox1.Controls.Add(TextBox1)
-        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Location = New Point(14, 16)
+        GroupBox1.Margin = New Padding(3, 4, 3, 4)
         GroupBox1.Name = "GroupBox1"
-        GroupBox1.Size = New Size(460, 342)
+        GroupBox1.Padding = New Padding(3, 4, 3, 4)
+        GroupBox1.Size = New Size(526, 456)
         GroupBox1.TabIndex = 33
         GroupBox1.TabStop = False
-        GroupBox1.Text = "Alta de empleado" ' 
-        ' DateTimePicker1
-        ' 
-        DateTimePicker1.Location = New Point(229, 176)
-        DateTimePicker1.Name = "DateTimePicker1"
-        DateTimePicker1.Size = New Size(19, 23)
-        DateTimePicker1.TabIndex = 30
+        GroupBox1.Text = "Alta de empleado"
         ' 
         ' DateTimePicker2
         ' 
-        DateTimePicker2.Location = New Point(433, 176)
+        DateTimePicker2.Location = New Point(495, 235)
+        DateTimePicker2.Margin = New Padding(3, 4, 3, 4)
         DateTimePicker2.Name = "DateTimePicker2"
-        DateTimePicker2.Size = New Size(19, 23)
+        DateTimePicker2.Size = New Size(21, 27)
         DateTimePicker2.TabIndex = 31
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(262, 235)
+        DateTimePicker1.Margin = New Padding(3, 4, 3, 4)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(21, 27)
+        DateTimePicker1.TabIndex = 30
         ' 
         ' Form2
         ' 
-        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(484, 428)
+        ClientSize = New Size(553, 571)
         Controls.Add(GroupBox1)
         Controls.Add(Button3)
         Controls.Add(Button2)
         Controls.Add(Button1)
+        Margin = New Padding(3, 4, 3, 4)
         Name = "Form2"
         Text = "Captura de personal"
         GroupBox1.ResumeLayout(False)
