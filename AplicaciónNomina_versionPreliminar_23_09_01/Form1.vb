c6227cc660d54e3c529c066d5a3acdf031dd3ffa
Fabian Cruz <fabian_cruza@hotmail.com>
2023-09-04 19:51:03 -0600

    Se agrego el Form2 captura de personal

--- AplicaciónNomina_versionPreliminar_23_09_01/Form1.vb
+++ AplicaciónNomina_versionPreliminar_23_09_01/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
-
+    Private Sub CapturaPersonalToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CapturaPersonalToolStripMenuItem.Click
+        Form2.Show()
+    End Sub
 End Class
---
+++ AplicaciónNomina_versionPreliminar_23_09_01/Form2.Designer.vb
@@ -0,0 +1,422 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        TextBox9 = New TextBox()
+        TextBox10 = New TextBox()
+        TextBox11 = New TextBox()
+        TextBox12 = New TextBox()
+        TextBox13 = New TextBox()
+        TextBox14 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        GroupBox1 = New GroupBox()
+        DateTimePicker1 = New DateTimePicker()
+        DateTimePicker2 = New DateTimePicker()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.CharacterCasing = CharacterCasing.Upper
+        TextBox1.Location = New Point(225, 29)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(227, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.CharacterCasing = CharacterCasing.Upper
+        TextBox2.Location = New Point(144, 62)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(308, 23)
+        TextBox2.TabIndex = 1
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.CharacterCasing = CharacterCasing.Upper
+        TextBox3.Location = New Point(144, 91)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(308, 23)
+        TextBox3.TabIndex = 2
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.CharacterCasing = CharacterCasing.Upper
+        TextBox4.Location = New Point(144, 120)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(308, 23)
+        TextBox4.TabIndex = 3
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.CharacterCasing = CharacterCasing.Upper
+        TextBox5.Location = New Point(144, 147)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(308, 23)
+        TextBox5.TabIndex = 4
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.CharacterCasing = CharacterCasing.Upper
+        TextBox6.Location = New Point(144, 176)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(79, 23)
+        TextBox6.TabIndex = 5
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.CharacterCasing = CharacterCasing.Upper
+        TextBox7.Location = New Point(348, 176)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(79, 23)
+        TextBox7.TabIndex = 6
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.CharacterCasing = CharacterCasing.Upper
+        TextBox8.Location = New Point(144, 205)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(104, 23)
+        TextBox8.TabIndex = 7
+        ' 
+        ' TextBox9
+        ' 
+        TextBox9.CharacterCasing = CharacterCasing.Upper
+        TextBox9.Location = New Point(348, 205)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(104, 23)
+        TextBox9.TabIndex = 8
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.CharacterCasing = CharacterCasing.Upper
+        TextBox10.Location = New Point(144, 234)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(104, 23)
+        TextBox10.TabIndex = 9
+        ' 
+        ' TextBox11
+        ' 
+        TextBox11.CharacterCasing = CharacterCasing.Upper
+        TextBox11.Location = New Point(348, 234)
+        TextBox11.Name = "TextBox11"
+        TextBox11.Size = New Size(104, 23)
+        TextBox11.TabIndex = 10
+        ' 
+        ' TextBox12
+        ' 
+        TextBox12.CharacterCasing = CharacterCasing.Upper
+        TextBox12.Location = New Point(144, 263)
+        TextBox12.Name = "TextBox12"
+        TextBox12.Size = New Size(104, 23)
+        TextBox12.TabIndex = 11
+        ' 
+        ' TextBox13
+        ' 
+        TextBox13.CharacterCasing = CharacterCasing.Upper
+        TextBox13.Location = New Point(348, 263)
+        TextBox13.Name = "TextBox13"
+        TextBox13.Size = New Size(104, 23)
+        TextBox13.TabIndex = 12
+        ' 
+        ' TextBox14
+        ' 
+        TextBox14.CharacterCasing = CharacterCasing.Upper
+        TextBox14.Location = New Point(144, 292)
+        TextBox14.Name = "TextBox14"
+        TextBox14.Size = New Size(104, 23)
+        TextBox14.TabIndex = 13
+        ' 
+        ' Label1
+        ' 
+        Label1.Location = New Point(87, 37)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(51, 15)
+        Label1.TabIndex = 14
+        Label1.Text = "Numero" ' 
+        ' Label2
+        ' 
+        Label2.BackColor = Color.FromArgb(CByte(255), CByte(255), CByte(128))
+        Label2.Location = New Point(144, 29)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 23)
+        Label2.TabIndex = 15
+        ' 
+        ' Label3
+        ' 
+        Label3.Location = New Point(87, 70)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(37, 15)
+        Label3.TabIndex = 16
+        Label3.Text = "CURP" ' 
+        ' Label4
+        ' 
+        Label4.Location = New Point(191, 37)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(28, 15)
+        Label4.TabIndex = 17
+        Label4.Text = "RFC" ' 
+        ' Label5
+        ' 
+        Label5.Location = New Point(40, 155)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(99, 15)
+        Label5.TabIndex = 20
+        Label5.Text = "Apellido materno" ' 
+        ' Label6
+        ' 
+        Label6.Location = New Point(44, 128)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(95, 15)
+        Label6.TabIndex = 19
+        Label6.Text = "Apellido paterno" ' 
+        ' Label7
+        ' 
+        Label7.Location = New Point(88, 99)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(51, 15)
+        Label7.TabIndex = 18
+        Label7.Text = "Nombre" ' 
+        ' Label8
+        ' 
+        Label8.Location = New Point(18, 213)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(120, 15)
+        Label8.TabIndex = 23
+        Label8.Text = "Ingreso diario normal" ' 
+        ' Label9
+        ' 
+        Label9.Location = New Point(263, 184)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(79, 15)
+        Label9.TabIndex = 22
+        Label9.Text = "Fecha de baja" ' 
+        ' Label10
+        ' 
+        Label10.Location = New Point(43, 184)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(96, 15)
+        Label10.TabIndex = 21
+        Label10.Text = "Fecha de ingreso" ' 
+        ' Label11
+        ' 
+        Label11.Location = New Point(263, 242)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(79, 15)
+        Label11.TabIndex = 26
+        Label11.Text = "Registro IMSS" ' 
+        ' Label12
+        ' 
+        Label12.Location = New Point(30, 242)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(108, 15)
+        Label12.TabIndex = 25
+        Label12.Text = "Otros ingresos fijos" ' 
+        ' Label13
+        ' 
+        Label13.Location = New Point(256, 213)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(86, 15)
+        Label13.TabIndex = 24
+        Label13.Text = "Viaticos diarios" ' 
+        ' Label14
+        ' 
+        Label14.Location = New Point(9, 300)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(129, 15)
+        Label14.TabIndex = 29
+        Label14.Text = "Salario diario integrado" ' 
+        ' Label15
+        ' 
+        Label15.Location = New Point(279, 271)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(63, 15)
+        Label15.TabIndex = 28
+        Label15.Text = "Porcentaje" ' 
+        ' Label16
+        ' 
+        Label16.Location = New Point(106, 271)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(33, 15)
+        Label16.TabIndex = 27
+        Label16.Text = "Obra" ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 360)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(133, 36)
+        Button1.TabIndex = 30
+        Button1.Text = "Guardar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(174, 360)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(133, 36)
+        Button2.TabIndex = 31
+        Button2.Text = "Limpiar"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(339, 360)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(133, 36)
+        Button3.TabIndex = 32
+        Button3.Text = "Cancelar"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(DateTimePicker2)
+        GroupBox1.Controls.Add(DateTimePicker1)
+        GroupBox1.Controls.Add(Label14)
+        GroupBox1.Controls.Add(Label15)
+        GroupBox1.Controls.Add(Label16)
+        GroupBox1.Controls.Add(Label11)
+        GroupBox1.Controls.Add(Label12)
+        GroupBox1.Controls.Add(Label13)
+        GroupBox1.Controls.Add(Label8)
+        GroupBox1.Controls.Add(Label9)
+        GroupBox1.Controls.Add(Label10)
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(Label6)
+        GroupBox1.Controls.Add(Label7)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(TextBox14)
+        GroupBox1.Controls.Add(TextBox13)
+        GroupBox1.Controls.Add(TextBox12)
+        GroupBox1.Controls.Add(TextBox11)
+        GroupBox1.Controls.Add(TextBox10)
+        GroupBox1.Controls.Add(TextBox9)
+        GroupBox1.Controls.Add(TextBox8)
+        GroupBox1.Controls.Add(TextBox7)
+        GroupBox1.Controls.Add(TextBox6)
+        GroupBox1.Controls.Add(TextBox5)
+        GroupBox1.Controls.Add(TextBox4)
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(460, 342)
+        GroupBox1.TabIndex = 33
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Alta de empleado" ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(229, 176)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(19, 23)
+        DateTimePicker1.TabIndex = 30
+        ' 
+        ' DateTimePicker2
+        ' 
+        DateTimePicker2.Location = New Point(433, 176)
+        DateTimePicker2.Name = "DateTimePicker2"
+        DateTimePicker2.Size = New Size(19, 23)
+        DateTimePicker2.TabIndex = 31
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(484, 428)
+        Controls.Add(GroupBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form2"
+        Text = "Captura de personal"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+End Class
