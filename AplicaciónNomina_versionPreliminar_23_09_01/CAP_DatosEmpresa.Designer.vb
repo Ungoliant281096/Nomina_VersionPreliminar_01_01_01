a6dadf5070dfb32752b4c015334047a4320b0cb7
Claudia Pérez <claudia.perez1703@outlook.com>
2023-09-07 13:18:29 -0600

    Se agregó Form CAP_DatosEmpresa

---
+++ AplicaciónNomina_versionPreliminar_23_09_01/CAP_DatosEmpresa.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CAP_DatosEmpresa
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(65, 47)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(93, 23)
+        Label1.TabIndex = 0
+        Label1.Text = "NOMBRE :"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(65, 120)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(51, 23)
+        Label2.TabIndex = 1
+        Label2.Text = "RFC :"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(390, 120)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(58, 23)
+        Label3.TabIndex = 2
+        Label3.Text = "AÑO :"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(92, 208)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(210, 23)
+        Label4.TabIndex = 3
+        Label4.Text = "NOMBRE DEL ARCHIVO :"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(166, 47)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(434, 27)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(166, 120)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(177, 27)
+        TextBox2.TabIndex = 5
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(475, 120)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 6
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(317, 205)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(283, 27)
+        TextBox4.TabIndex = 7
+        ' 
+        ' CAP_DatosEmpresa
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(677, 310)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "CAP_DatosEmpresa"
+        Text = "CAP_DatosEmpresa"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+End Class
---
+++ AplicaciónNomina_versionPreliminar_23_09_01/CAP_DatosEmpresa.vb
@@ -0,0 +1,3 @@
+﻿Public Class CAP_DatosEmpresa
+
+End Class
