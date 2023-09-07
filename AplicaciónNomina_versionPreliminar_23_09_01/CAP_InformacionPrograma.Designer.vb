c2cdef4d43d991a25d06cc01a9b1fbff1831fd5b
Claudia Pérez <claudia.perez1703@outlook.com>
2023-09-07 14:46:15 -0600

    Se agregó Form CAP_InformacionPrograma

---
+++ AplicaciónNomina_versionPreliminar_23_09_01/CAP_InformacionPrograma.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CAP_InformacionPrograma
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        Label14 = New Label()
+        Label13 = New Label()
+        Label12 = New Label()
+        Label11 = New Label()
+        Label10 = New Label()
+        Label9 = New Label()
+        Label8 = New Label()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Label14)
+        GroupBox1.Controls.Add(Label13)
+        GroupBox1.Controls.Add(Label12)
+        GroupBox1.Controls.Add(Label11)
+        GroupBox1.Controls.Add(Label10)
+        GroupBox1.Controls.Add(Label9)
+        GroupBox1.Controls.Add(Label8)
+        GroupBox1.Controls.Add(Label7)
+        GroupBox1.Controls.Add(Label6)
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(48, 15)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(704, 421)
+        GroupBox1.TabIndex = 15
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Propiedades"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label14.Location = New Point(312, 362)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(84, 20)
+        Label14.TabIndex = 13
+        Label14.Text = "win 7, 8, 10"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label13.Location = New Point(312, 308)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(83, 20)
+        Label13.TabIndex = 12
+        Label13.Text = "x64/ 86bits"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label12.Location = New Point(312, 253)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(110, 20)
+        Label12.TabIndex = 11
+        Label12.Text = "4.0 kB en disco"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label11.Location = New Point(312, 198)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(233, 20)
+        Label11.TabIndex = 10
+        Label11.Text = "SACMAG DE MÉXICO S.A DE C.V."
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label10.Location = New Point(312, 146)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(99, 20)
+        Label10.TabIndex = 9
+        Label10.Text = "12 Junio 2023"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label9.Location = New Point(312, 88)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(62, 20)
+        Label9.TabIndex = 8
+        Label9.Text = "2023.5.1"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label8.Location = New Point(312, 36)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(66, 20)
+        Label8.TabIndex = 7
+        Label8.Text = "Captura"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(36, 362)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(93, 20)
+        Label7.TabIndex = 6
+        Label7.Text = "Compatible:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(36, 308)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(143, 20)
+        Label6.TabIndex = 5
+        Label6.Text = "Sistema basado en:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(36, 253)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(106, 20)
+        Label5.TabIndex = 4
+        Label5.Text = "Tamaño total:"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(36, 198)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(56, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "Editor:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(36, 146)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(154, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Última modificación:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(36, 88)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(65, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Versión:"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(36, 36)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(71, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Nombre:"
+        ' 
+        ' CAP_InformacionPrograma
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(GroupBox1)
+        Name = "CAP_InformacionPrograma"
+        Text = "CAP_InformacionPrograma"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
