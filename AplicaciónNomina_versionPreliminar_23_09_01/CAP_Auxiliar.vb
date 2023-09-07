9073e33a6c13b52cd12f1e5143d60ed3aa9de2a1
Claudia Pérez <claudia.perez1703@outlook.com>
2023-09-07 13:05:49 -0600

    Se agregó Form CAP_Auxiliar

---
+++ AplicaciónNomina_versionPreliminar_23_09_01/CAP_Auxiliar.vb
@@ -0,0 +1,5 @@
+﻿Public Class CAP_Auxiliar
+    Private Sub CopiarSelecciónCtrlCToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopiarSelecciónCtrlCToolStripMenuItem.Click
+
+    End Sub
+End Class
