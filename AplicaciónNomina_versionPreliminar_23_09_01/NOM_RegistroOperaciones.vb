28d3077a09906ee4a3c8b012ee0803d655ae4a2b
Claudia Pérez <claudia.perez1703@outlook.com>
2023-09-07 08:49:35 -0600

    Se agregó Form NOM_RegistroOperaciones

---
+++ AplicaciónNomina_versionPreliminar_23_09_01/NOM_RegistroOperaciones.vb
@@ -0,0 +1,5 @@
+﻿Public Class NOM_RegistroOperaciones
+    Private Sub AlfabeticoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AlfabeticoToolStripMenuItem.Click
+
+    End Sub
+End Class
