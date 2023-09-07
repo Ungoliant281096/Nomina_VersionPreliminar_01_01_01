1ec6b00b245225f45308bc022c1534016795b220
Claudia Pérez <claudia.perez1703@outlook.com>
2023-09-07 12:58:53 -0600

    Se agregó Form CAP_ContabilidadGeneralAnexos

---
+++ AplicaciónNomina_versionPreliminar_23_09_01/CAP_ContabilidadGeneralAccesso.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CAP_ContabilidadGeneralAccesso
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        AnexoToolStripMenuItem = New ToolStripMenuItem()
+        EdiciónToolStripMenuItem = New ToolStripMenuItem()
+        OrdenarToolStripMenuItem = New ToolStripMenuItem()
+        ImprimirToolStripMenuItem = New ToolStripMenuItem()
+        CopiarTodoToolStripMenuItem = New ToolStripMenuItem()
+        SeleccionarTodoToolStripMenuItem = New ToolStripMenuItem()
+        AlfabéticamenteToolStripMenuItem = New ToolStripMenuItem()
+        NuméricamenteToolStripMenuItem = New ToolStripMenuItem()
+        DataGridView1 = New DataGridView()
+        MenuStrip1.SuspendLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.ImageScalingSize = New Size(20, 20)
+        MenuStrip1.Items.AddRange(New ToolStripItem() {AnexoToolStripMenuItem, EdiciónToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(626, 28)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' AnexoToolStripMenuItem
+        ' 
+        AnexoToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {OrdenarToolStripMenuItem, ImprimirToolStripMenuItem})
+        AnexoToolStripMenuItem.Name = "AnexoToolStripMenuItem"
+        AnexoToolStripMenuItem.Size = New Size(65, 24)
+        AnexoToolStripMenuItem.Text = "Anexo"
+        ' 
+        ' EdiciónToolStripMenuItem
+        ' 
+        EdiciónToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {CopiarTodoToolStripMenuItem, SeleccionarTodoToolStripMenuItem})
+        EdiciónToolStripMenuItem.Name = "EdiciónToolStripMenuItem"
+        EdiciónToolStripMenuItem.Size = New Size(72, 24)
+        EdiciónToolStripMenuItem.Text = "Edición"
+        ' 
+        ' OrdenarToolStripMenuItem
+        ' 
+        OrdenarToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {AlfabéticamenteToolStripMenuItem, NuméricamenteToolStripMenuItem})
+        OrdenarToolStripMenuItem.Name = "OrdenarToolStripMenuItem"
+        OrdenarToolStripMenuItem.Size = New Size(224, 26)
+        OrdenarToolStripMenuItem.Text = "Ordenar"
+        ' 
+        ' ImprimirToolStripMenuItem
+        ' 
+        ImprimirToolStripMenuItem.Name = "ImprimirToolStripMenuItem"
+        ImprimirToolStripMenuItem.Size = New Size(224, 26)
+        ImprimirToolStripMenuItem.Text = "Imprimir"
+        ' 
+        ' CopiarTodoToolStripMenuItem
+        ' 
+        CopiarTodoToolStripMenuItem.Name = "CopiarTodoToolStripMenuItem"
+        CopiarTodoToolStripMenuItem.Size = New Size(291, 26)
+        CopiarTodoToolStripMenuItem.Text = "Copiar todo                 Ctrl + S"
+        ' 
+        ' SeleccionarTodoToolStripMenuItem
+        ' 
+        SeleccionarTodoToolStripMenuItem.Name = "SeleccionarTodoToolStripMenuItem"
+        SeleccionarTodoToolStripMenuItem.Size = New Size(291, 26)
+        SeleccionarTodoToolStripMenuItem.Text = "Copiar selección          Ctrl + C"
+        ' 
+        ' AlfabéticamenteToolStripMenuItem
+        ' 
+        AlfabéticamenteToolStripMenuItem.Name = "AlfabéticamenteToolStripMenuItem"
+        AlfabéticamenteToolStripMenuItem.Size = New Size(224, 26)
+        AlfabéticamenteToolStripMenuItem.Text = "Alfabéticamente"
+        ' 
+        ' NuméricamenteToolStripMenuItem
+        ' 
+        NuméricamenteToolStripMenuItem.Name = "NuméricamenteToolStripMenuItem"
+        NuméricamenteToolStripMenuItem.Size = New Size(224, 26)
+        NuméricamenteToolStripMenuItem.Text = "Numéricamente"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(33, 54)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.RowTemplate.Height = 29
+        DataGridView1.Size = New Size(543, 437)
+        DataGridView1.TabIndex = 1
+        ' 
+        ' CAP_ContabilidadGeneralAccesso
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(626, 554)
+        Controls.Add(DataGridView1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "CAP_ContabilidadGeneralAccesso"
+        Text = "CAP_ContabilidadGeneralAccesso"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AnexoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OrdenarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ImprimirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EdiciónToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopiarTodoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SeleccionarTodoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AlfabéticamenteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NuméricamenteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ AplicaciónNomina_versionPreliminar_23_09_01/CAP_ContabilidadGeneralAccesso.vb
@@ -0,0 +1,9 @@
+﻿Public Class CAP_ContabilidadGeneralAccesso
+    Private Sub CAP_ContabilidadGeneralAccesso_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub CopiarTodoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopiarTodoToolStripMenuItem.Click
+
+    End Sub
+End Class
