9dd4b50d35596026bbf53968c2746226b35fc6c3
Claudia Pérez <claudia.perez1703@outlook.com>
2023-09-08 10:37:54 -0600

    Se agregó información en form CAP_AjusteImpresionCheques

--- AplicaciónNomina_versionPreliminar_23_09_01/CAP_AjusteImpresionCheques.Designer.vb
+++ AplicaciónNomina_versionPreliminar_23_09_01/CAP_AjusteImpresionCheques.Designer.vb
@@ -30,6 +30,19 @@ Partial Class CAP_AjusteImpresionCheques
         SalirToolStripMenuItem = New ToolStripMenuItem()
         TextBox1 = New TextBox()
         DataGridView1 = New DataGridView()
+        ColFecha = New DataGridViewTextBoxColumn()
+        ColBenef = New DataGridViewTextBoxColumn()
+        ColImpteNum = New DataGridViewTextBoxColumn()
+        ColImpteLetra = New DataGridViewTextBoxColumn()
+        ColConcep = New DataGridViewTextBoxColumn()
+        ColInicioApli = New DataGridViewTextBoxColumn()
+        ColSumasI = New DataGridViewTextBoxColumn()
+        ColCta = New DataGridViewTextBoxColumn()
+        ColSubCta = New DataGridViewTextBoxColumn()
+        ColNomCtaSubcta = New DataGridViewTextBoxColumn()
+        ColParc = New DataGridViewTextBoxColumn()
+        ColDebe = New DataGridViewTextBoxColumn()
+        ColHaber = New DataGridViewTextBoxColumn()
         MenuStrip1.SuspendLayout()
         CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
@@ -54,25 +67,25 @@ Partial Class CAP_AjusteImpresionCheques
         ' ArchivarToolStripMenuItem
         ' 
         ArchivarToolStripMenuItem.Name = "ArchivarToolStripMenuItem"
-        ArchivarToolStripMenuItem.Size = New Size(224, 26)
+        ArchivarToolStripMenuItem.Size = New Size(208, 26)
         ArchivarToolStripMenuItem.Text = "Archivar"
         ' 
         ' ImpresiónDatosToolStripMenuItem
         ' 
         ImpresiónDatosToolStripMenuItem.Name = "ImpresiónDatosToolStripMenuItem"
-        ImpresiónDatosToolStripMenuItem.Size = New Size(224, 26)
+        ImpresiónDatosToolStripMenuItem.Size = New Size(208, 26)
         ImpresiónDatosToolStripMenuItem.Text = "Impresión datos"
         ' 
         ' PruebaImpresiónToolStripMenuItem
         ' 
         PruebaImpresiónToolStripMenuItem.Name = "PruebaImpresiónToolStripMenuItem"
-        PruebaImpresiónToolStripMenuItem.Size = New Size(224, 26)
+        PruebaImpresiónToolStripMenuItem.Size = New Size(208, 26)
         PruebaImpresiónToolStripMenuItem.Text = "Prueba impresión"
         ' 
         ' SalirToolStripMenuItem
         ' 
         SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
-        SalirToolStripMenuItem.Size = New Size(224, 26)
+        SalirToolStripMenuItem.Size = New Size(208, 26)
         SalirToolStripMenuItem.Text = "Salir"
         ' 
         ' TextBox1
@@ -85,12 +98,104 @@ Partial Class CAP_AjusteImpresionCheques
         ' DataGridView1
         ' 
         DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {ColFecha, ColBenef, ColImpteNum, ColImpteLetra, ColConcep, ColInicioApli, ColSumasI, ColCta, ColSubCta, ColNomCtaSubcta, ColParc, ColDebe, ColHaber})
         DataGridView1.Location = New Point(24, 95)
         DataGridView1.Name = "DataGridView1"
         DataGridView1.RowHeadersWidth = 51
         DataGridView1.RowTemplate.Height = 29
         DataGridView1.Size = New Size(408, 452)
         DataGridView1.TabIndex = 2
+        ' 
+        ' ColFecha
+        ' 
+        ColFecha.HeaderText = "FECHA"
+        ColFecha.MinimumWidth = 6
+        ColFecha.Name = "ColFecha"
+        ColFecha.Width = 125
+        ' 
+        ' ColBenef
+        ' 
+        ColBenef.HeaderText = "BENEFICIARIO "
+        ColBenef.MinimumWidth = 6
+        ColBenef.Name = "ColBenef"
+        ColBenef.Width = 125
+        ' 
+        ' ColImpteNum
+        ' 
+        ColImpteNum.HeaderText = "IMPORTE EN NÚMERO"
+        ColImpteNum.MinimumWidth = 6
+        ColImpteNum.Name = "ColImpteNum"
+        ColImpteNum.Width = 125
+        ' 
+        ' ColImpteLetra
+        ' 
+        ColImpteLetra.HeaderText = "IMPORTE EN LETRA"
+        ColImpteLetra.MinimumWidth = 6
+        ColImpteLetra.Name = "ColImpteLetra"
+        ColImpteLetra.Width = 125
+        ' 
+        ' ColConcep
+        ' 
+        ColConcep.HeaderText = "CONCEPTO"
+        ColConcep.MinimumWidth = 6
+        ColConcep.Name = "ColConcep"
+        ColConcep.Width = 125
+        ' 
+        ' ColInicioApli
+        ' 
+        ColInicioApli.HeaderText = "INICIO APLICACIÓN"
+        ColInicioApli.MinimumWidth = 6
+        ColInicioApli.Name = "ColInicioApli"
+        ColInicioApli.Width = 125
+        ' 
+        ' ColSumasI
+        ' 
+        ColSumasI.HeaderText = "SUMAS IGUALES"
+        ColSumasI.MinimumWidth = 6
+        ColSumasI.Name = "ColSumasI"
+        ColSumasI.Width = 125
+        ' 
+        ' ColCta
+        ' 
+        ColCta.HeaderText = "CUENTA"
+        ColCta.MinimumWidth = 6
+        ColCta.Name = "ColCta"
+        ColCta.Width = 125
+        ' 
+        ' ColSubCta
+        ' 
+        ColSubCta.HeaderText = "SUBCUENTA"
+        ColSubCta.MinimumWidth = 6
+        ColSubCta.Name = "ColSubCta"
+        ColSubCta.Width = 125
+        ' 
+        ' ColNomCtaSubcta
+        ' 
+        ColNomCtaSubcta.HeaderText = "NOMBRE CTA-SUBCTA"
+        ColNomCtaSubcta.MinimumWidth = 6
+        ColNomCtaSubcta.Name = "ColNomCtaSubcta"
+        ColNomCtaSubcta.Width = 125
+        ' 
+        ' ColParc
+        ' 
+        ColParc.HeaderText = "PARCIAL"
+        ColParc.MinimumWidth = 6
+        ColParc.Name = "ColParc"
+        ColParc.Width = 125
+        ' 
+        ' ColDebe
+        ' 
+        ColDebe.HeaderText = "DEBE"
+        ColDebe.MinimumWidth = 6
+        ColDebe.Name = "ColDebe"
+        ColDebe.Width = 125
+        ' 
+        ' ColHaber
+        ' 
+        ColHaber.HeaderText = "HABER"
+        ColHaber.MinimumWidth = 6
+        ColHaber.Name = "ColHaber"
+        ColHaber.Width = 125
         ' 
         ' CAP_AjusteImpresionCheques
         ' 
@@ -118,4 +223,17 @@ Partial Class CAP_AjusteImpresionCheques
     Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ColFecha As DataGridViewTextBoxColumn
+    Friend WithEvents ColBenef As DataGridViewTextBoxColumn
+    Friend WithEvents ColImpteNum As DataGridViewTextBoxColumn
+    Friend WithEvents ColImpteLetra As DataGridViewTextBoxColumn
+    Friend WithEvents ColConcep As DataGridViewTextBoxColumn
+    Friend WithEvents ColInicioApli As DataGridViewTextBoxColumn
+    Friend WithEvents ColSumasI As DataGridViewTextBoxColumn
+    Friend WithEvents ColCta As DataGridViewTextBoxColumn
+    Friend WithEvents ColSubCta As DataGridViewTextBoxColumn
+    Friend WithEvents ColNomCtaSubcta As DataGridViewTextBoxColumn
+    Friend WithEvents ColParc As DataGridViewTextBoxColumn
+    Friend WithEvents ColDebe As DataGridViewTextBoxColumn
+    Friend WithEvents ColHaber As DataGridViewTextBoxColumn
 End Class
